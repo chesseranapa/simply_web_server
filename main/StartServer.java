9792430c4cbee72468a687713c76db9f7c4dfc88
Alexey Evdokimov <chesseranapa@gmail.com>
2019-06-30 01:21:04 +0300

    убрал лишние импорты

--- main/StartServer.java
+++ main/StartServer.java
@@ -5,11 +5,7 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.SessionManager;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.session.HashSessionIdManager;
-import org.eclipse.jetty.server.session.HashSessionManager;
-import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
